74bf25e9e4c0ab18b8708f4c7b12752abc7309da
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-29 20:58:10 +0300

    add player animation

--- Assets/Project/Sprites/Animation/Player/PlayerAnimation.cs
+++ Assets/Project/Sprites/Animation/Player/PlayerAnimation.cs
@@ -1,19 +1,35 @@
+using PixelTower.Movement;
 using UnityEngine;
 
 namespace PixelTower.Animation
 {
     public class PlayerAnimation : MonoBehaviour
     {
-        [SerializeField] private Animator _animator;
+        [Header("Management")]
         [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private GroundLocator _groundLocator;
+
+        [Header("Viewer")]
+        [SerializeField] private Animator _animator;
         [SerializeField] private SpriteRenderer _renderer;
 
         private readonly int _running = Animator.StringToHash("IsRunning");
+        private readonly int _jumping = Animator.StringToHash("IsJumping");
+        private readonly int _falling = Animator.StringToHash("IsFalling");
 
         private void Update()
         {
-            _animator.SetBool(_running, _rigidbody.velocity != Vector2.zero);
-            _renderer.flipX = _rigidbody.velocity.x <= 0;
+            if (_rigidbody.velocity.x != 0)
+                _renderer.flipX = _rigidbody.velocity.x <= 0;
+
+            if (_groundLocator.IsGround == false && _rigidbody.velocity.y > 0) 
+                _animator.SetBool(_jumping, true);
+
+            if (_groundLocator.IsGround == false && _rigidbody.velocity.y < 0)
+                _animator.SetBool(_falling, true);
+
+            if (_groundLocator.IsGround == true && _rigidbody.velocity.x != 0)
+                _animator.SetBool(_running, _rigidbody.velocity != Vector2.zero);
         }
     }
 }
