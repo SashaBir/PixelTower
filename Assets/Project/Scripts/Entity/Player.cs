f27e9676f770593342e795b5fc49ddefd93b4bb6
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-30 19:14:59 +0300

    add explosion

---
+++ Assets/Project/Scripts/Entity/Player.cs
@@ -0,0 +1,23 @@
+﻿using PixelTower.Addition;
+using PixelTower.Environment;
+using UnityEngine;
+
+namespace PixelTower.Entity
+{
+    public class Player : MonoBehaviour, IDamageable, IRepelable
+    {
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private float _health;
+
+        public void Damage(float damage)
+        {
+            _health -= damage;
+        }
+
+        public void Repel(Vector2 direction, float distance)
+        {
+            float force = PhysicsExpansion.CalculateForceNoMass(distance, _rigidbody.gravityScale);
+            _rigidbody.velocity = direction * force;
+        }
+    }
+}
--- Assets/Project/Scripts/Weapon/Projectile.cs
+++ Assets/Project/Scripts/Weapon/Projectile.cs
@@ -1,4 +1,5 @@
 ﻿using PixelTower.Entity;
+using PixelTower.Environment;
 using UnityEngine;
 
 namespace PixelTower.Weapon
@@ -7,12 +8,57 @@ namespace PixelTower.Weapon
     {
         [SerializeField] private float _damage;
         [SerializeField] private float _health;
+        [SerializeField] private float _destroyAfterSpawning;
+        [SerializeField] private float _distanceRepeled;
+        [SerializeField] private float _radiusDetected;
+
+        private void Start()
+        {
+            Destroy(gameObject, _destroyAfterSpawning);
+        }
+
+        private void OnDestroy()
+        {
+            Explosion();
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.transform.TryGetComponent(out IRepelable repelable) == true)
+            {
+                var direction = (collision.transform.position - transform.position).normalized;
+                repelable.Repel(direction, _distanceRepeled);
+            }
+
+            if (collision.transform.TryGetComponent(out IDamageable damageable) == true)
+                damageable.Damage(_damage);
+
+
+            Destroy(gameObject);
+        }
 
         public void Damage(float damage)
         {
             _health -= damage;
             if (_health <= 0)
                 Destroy(gameObject);
+        }
+
+        private void Explosion()
+        {
+            var hits = Physics2D.CircleCastAll(transform.position, _radiusDetected, Vector2.zero);
+            foreach (var hit in hits)
+            {
+                if (hit.transform == null)
+                    continue;
+
+                var direction = hit.transform.position - transform.position;
+                if (hit.transform.TryGetComponent(out IRepelable repelable) == true)
+                    repelable.Repel(direction.normalized, _distanceRepeled);
+
+                if (hit.transform.TryGetComponent(out IDamageable damageable) == true)
+                    damageable.Damage(_damage);
+            }
         }
     }
 }
