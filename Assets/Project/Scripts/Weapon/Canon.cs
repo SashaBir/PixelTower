29ed2d77ddbdb7d2b200db7024f4f4ffd7305900
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2023-04-30 11:49:08 +0300

    add cannon

---
+++ Assets/Project/Scripts/Weapon/Canon.cs
@@ -0,0 +1,53 @@
+using PixelTower.Addition;
+using System.Collections;
+using UnityEngine;
+
+namespace PixelTower.Weapon
+{
+    public class Canon : MonoBehaviour
+    {
+        [Header("Attcking")]
+        [SerializeField] private Rigidbody2D _projectile;
+        [SerializeField] private Transform _muzzle;
+        [SerializeField] private Vector2 _direction;
+        [SerializeField] private float _distance;
+        [SerializeField] private float _delay;
+
+        [Header("Viewer")]
+        [SerializeField] private Animator _animator;
+
+        private readonly int _attacking = Animator.StringToHash("Attacking");
+
+        private void Start()
+        {
+            StartCoroutine(Attack());
+        }
+
+        private IEnumerator Attack()
+        {
+            //yield return new WaitForSeconds(Random.Range(3, 5f));
+
+            var delay = new WaitForSeconds(_delay);
+            do
+            {
+                yield return delay;
+
+                Shoot();
+                AnimateAttack();
+            }
+            while (true);
+        }
+
+        private void Shoot()
+        {
+            var projectile = Instantiate(_projectile, _muzzle.position, _muzzle.rotation);
+            var force = PhysicsExpansion.CalculateForceNoMass(_distance, projectile.gravityScale);
+            projectile.velocity = _direction * force;
+        }
+
+        private void AnimateAttack()
+        {
+            _animator.SetTrigger(_attacking);
+        }
+    }
+}
